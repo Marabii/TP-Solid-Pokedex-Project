24e4aeb6723eedceaeba427e1fdf4c28164b09cd
Hamza Dadda <109368243+Marabii@users.noreply.github.com>
2025-01-25 16:39:18 +0100

    Update LocalizedPropertyProviderInterface.java

--- src/main/java/com/pokedex/tp_pokedex/services/LocalizedPropertyProviderInterface.java
+++ src/main/java/com/pokedex/tp_pokedex/services/LocalizedPropertyProviderInterface.java
@@ -1,22 +1,7 @@
 package com.pokedex.tp_pokedex.services;
 
-/**
- * Interface for services that can be localized (i.e., support multiple locales).
- * Extends the basic property provider interface.
- */
+
 public interface LocalizedPropertyProviderInterface extends PropertyProviderInterface {
-
-    /**
-     * Sets the locale to use for string properties provided by the service.
-     *
-     * @param localeCode the locale code (e.g., "en", "fr")
-     */
     void setStringPropertyLocale(String localeCode);
-
-    /**
-     * Gets the locale that is currently used by the service.
-     *
-     * @return the current locale code
-     */
     String getStringPropertyLocale();
 }
